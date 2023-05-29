659ce092ac8359c2bf9b6d18b70194b38ff0f7e0
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-29 19:54:23 +0000

    create directories

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+ reducer: {
+   // Define a top-level state field named `todos`, handled by `todosReducer`
+ }
+});
+
+export default store;
