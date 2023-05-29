eefb01c7fc9407fee81af1adea54d925da9e32ce
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-29 20:21:27 +0000

    fix file path error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
- reducer: {
-   // Define a top-level state field named `todos`, handled by `todosReducer`
- }
+  reducer: {
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+  }
 });
 
 export default store;
