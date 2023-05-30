1a8aff192f1b731ce68100f52421116a66ec5af6
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 01:42:41 +0000

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
 
