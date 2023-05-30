a3d554a3ae40c9559d8a993fe9ae5e00e4147620
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-30 01:11:39 +0000

    set up routing with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className=']'>
-      <Login />
+    <div className=''>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+
     </div>
   )
 }
