811a3598eb1cbe2b8a6deb70f3c00667a46930bf
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-31 14:57:19 +0000

    create dynamic routing for all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import Dashboard from './Pages/Dashboard/Dashboard';
-import Login from './Pages/Login/Login'
-import Register from './Pages/Register/Register'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AllRides from './Pages/AllRides';
-import Favorites from './Pages/Favorites';
-import AddRide from './Pages/AddRide';
-import DeleteRide from './Pages/DeleteRide';
-import NotFound from './Pages/NotFound/NotFound';
+import ProjectRouter from "./components/Router/Router"
 
 function App() {
 
   return (
     <div className=''>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/my-ride' element={<AllRides />} />
-          <Route path='/favorite' element={<Favorites />} />
-          <Route path='/add-ride' element={<AddRide />} />
-          <Route path='/delete-ride' element={<DeleteRide />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Router>
-
+      <ProjectRouter />
     </div>
   )
 }
