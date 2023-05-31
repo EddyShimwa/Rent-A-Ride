9a2b0d43b9554ba72d14347549974ae960bb8077
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-31 14:00:33 +0000

    rename home to dashboard

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/SideBar/Sidebar";
+
+const Dashboard = () => {
+    return (
+      <div>
+        <div>
+          <Sidebar />
+        </div>
+    </div>
+  );
+};
+
+export default Dashboard;
