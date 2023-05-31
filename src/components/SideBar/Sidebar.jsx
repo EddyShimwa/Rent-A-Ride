5404fc11495275437a611cca52f9720caaee6a04
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-31 14:00:07 +0000

    add side navigation bar

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,83 +1,60 @@
 import { useState } from 'react'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-// import SidebarData from './SidebarData'
-import { IconContext } from 'react-icons'
-import { AiFillHome } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
-import { AiFillFileAdd } from "react-icons/ai";
-import { AiFillDelete } from "react-icons/ai";
+import { SlMenu } from 'react-icons/sl'
+import { BiImageAdd } from 'react-icons/bi'
+import { AiFillCar } from 'react-icons/ai'
+import { RiLogoutCircleRLine } from 'react-icons/ri'
+import { AiFillDelete } from 'react-icons/ai'
+import { CgClose } from 'react-icons/cg'
 
 const Sidebar = () => {
-  const [sidebar, setSidebar] = useState(false)
-
-  const showSidebar = () => setSidebar(!sidebar)
-
-  const SidebarData = [
-    {
-      title: "My Ride",
-      path: "/my-ride",
-      icon: AiFillHome,
-      cName: "nav-text"
-    },
-
-    {
-      title: "My Favorite",
-      path: "/favorite",
-      icon: FaHeart,
-      cName: "nav-text"
-    },
-
-    {
-      title: "Add Ride",
-      path: "/add-ride",
-      icon: AiFillFileAdd,
-      cName: "nav-text"
-    },
-
-    {
-      title: "Delete Ride",
-      path: "/delete-ride",
-      icon: AiFillDelete,
-      cName: "nav-text"
-    }
-
-  ];
+  const [show, setShow] = useState(false);
 
   return (
-    <div>
-      <IconContext.Provider value={{ color: '#333' }}>
-        <div className='navbar'>
-          <Link to='/' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
+    <main className={show ? 'space-toggle' : null}>
+      <header className={`header ${show ? 'space-toggle' : null}`}>
+        <div className='header-toggle' onClick={() => setShow(!show)}>
+          {show ? <CgClose className='header-toggle-icon' /> : <SlMenu className='header-toggle-icon' />}
         </div>
+      </header>
 
-        <nav className={sidebar ? 'nav-menu-active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle'>
-              <Link to='/' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
+      <aside className={`sidebar ${show ? 'show' : null}`}>
+        <nav className='nav'>
+          <div className='sidebar-items-container'>
+
+            <Link to='/my-ride' className='nav-logo active'>
+              <AiFillCar className='nav-link-icon' />
+              <span className='nav-logo-name'>My Rides</span>
+            </Link>
+
+            <div className='nav-list'>
+              <Link to='/favorite' className='nav-link'>
+                <FaHeart className='nav-link-icon' />
+                <span className='nav-link-name'>My Favorite</span>
               </Link>
-            </li>
+              <Link to='/add-ride' className='nav-link'>
+                <BiImageAdd className='nav-link-icon' />
+                <span className='nav-link-name'>Add Ride</span>
+              </Link>
+              <Link to='/delete-ride' className='nav-link'>
+                <AiFillDelete className='nav-link-icon' />
+                <span className='nav-link-name'>Delete Ride</span>
+              </Link>
+            </div>
+          </div>
 
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
+          <Link to='/login' className='nav-link'>
+            <RiLogoutCircleRLine className='nav-link-icon' />
+            <span className='nav-link-name'>Logout</span>
+          </Link>
         </nav>
-      </IconContext.Provider>
-    </div>
-  )
+      </aside>
+
+      <h1>Content</h1>
+    </main>
+  );
 }
 
 export default Sidebar
