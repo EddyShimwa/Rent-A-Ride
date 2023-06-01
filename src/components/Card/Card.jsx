9caf004ed1669f50fba1cdc1a43aa5f9750332ea
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-01 10:52:45 +0000

    define prop type object to specify the expected prop types for those used props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import './Card.css';
+import PropTypes from 'prop-types';
 
-const Card = ({ title, description, imageUrl, price, rating, time }) => {
+const Card = ({ title, description, price, rating, time }) => {
   return (
     <div className="card">
       <div className="card-content">
         {/* <div className='card-header'>
          <h2 className="card-title">{title}</h2>
-         <BsSearch className='search-icon' />
         </div> */}
         <img src="https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1583&q=80" alt={title} className="card-image" />
         <div className='card-footer'>
@@ -20,6 +20,15 @@ const Card = ({ title, description, imageUrl, price, rating, time }) => {
       </div>
     </div>
   );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
 };
 
 export default Card;
