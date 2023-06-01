23069c3ea224545421e8f29d30566271ac3352d5
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-01 09:50:46 +0000

    update these files

--- src/Pages/AllRides.jsx
+++ src/Pages/AllRides.jsx
@@ -1,7 +1,16 @@
+import Card from "../components/Card/Card"
 
 const AllRides = () => {
   return (
     <div>
+      <Card
+        title="Houses"
+        description="Beautiful family apartment"
+        price = "$1000"
+        imageUrl="https://example.com/image.jpg"
+        rating="4.5"
+        time="per week"
+      />
     </div>
   )
 }
