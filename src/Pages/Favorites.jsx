95acbbbd5602f451795e04a570cd7942356798b4
EddyShimwa <shedpray16@gmail.com>
2023-06-02 14:44:16 +0200

    add dummy favorites cars

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-const Favorites = ({favorites}) => {
+
+const Favorites = () => {
+
+  const favorites = [
+    { name: 'Favorite 1', description: 'Description 1' },
+    { name: 'Favorite 2', description: 'Description 2' },
+  ];
+
+  if (!favorites || favorites.length === 0) {
+    return <div>No favorites available</div>;
+  }
+
   return (
     <div>
       <h1>Favorites Rides</h1>
       <ul>
         {favorites.map((item, index) => (
-          <li key={index}>{/* favorite item details */}</li>
+          <li key={index}>
+            <p>{item.name}</p>
+            <p>{item.description}</p>
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Favorites
+export default Favorites;
+
+
+
+
+
+
+
+
+
+
+
