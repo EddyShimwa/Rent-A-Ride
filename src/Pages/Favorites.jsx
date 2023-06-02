185cf48d8897f3b613753318ab32dbcf81213e12
EddyShimwa <shedpray16@gmail.com>
2023-06-02 15:52:39 +0200

    update the classes for favorites

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -4,8 +4,8 @@ import "../Pages/favorites.css"
 
 const Favorites = () => {
   const favorites = [
-    { name: 'Favorite 1', description: 'Description 1' },
-    { name: 'Favorite 2', description: 'Description 2' },
+    { name: 'Favorite 1', description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo ' },
+    { name: 'Favorite 2', description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo ' },
   ];
 
   if (!favorites || favorites.length === 0) {
@@ -15,11 +15,12 @@ const Favorites = () => {
   return (
     <div className="favorites-container">
       <h1 className="favz-header">Favorites Rides</h1>
-      <ul>
+      <ul className='favorite-list'>
         {favorites.map((item, index) => (
-          <li key={index} className="favorite-list">
-            <p className="car-name">{item.name}</p>
+          <li key={index} className="favorite-list-item">
+            <h2 className="car-name">{item.name}</h2>
             <p className="car-description">{item.description}</p>
+            <button className='remove'>Remove from Favorites</button>
           </li>
         ))}
       </ul>
