7deb18e739a64fb4ce8fb5a71b0653052ce82a71
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-05 23:56:37 +0200

    Add data to the detailspage with props

--- src/Pages/Details/DetailsPage.jsx
+++ src/Pages/Details/DetailsPage.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { BsSearch } from 'react-icons/bs';
+import PropTypes from 'prop-types';
 import './DetailsPage.css'
 import { HiOutlineChevronLeft, HiOutlineChevronDown } from 'react-icons/hi';
 import ProfileImage from '../../assets/profile-image.png'
 import { Link } from 'react-router-dom';
-import StarRating from '../../components/star-rating/StarRating';
+//import StarRating from '../../components/star-rating/StarRating';
 
-const DetailsPage = () => {
+const DetailsPage = ({currentItems}) => {
   const [isFavorite, setIsFavorite] = useState(false);
   
   const handleAddToFavorites = () => {
@@ -14,18 +15,29 @@ const DetailsPage = () => {
   };
 
   return (
-    <div className="details-page-container">
+  <div className="details-page-container">
+    {currentItems
+          && currentItems.map((item) => (
+            <div
+            className="reservation-card"
+            key={item.id}
+            role="button"
+            tabIndex={0}
+            onClick={() => handleAddToFavorites(item.id)}
+          >
       <div className="details-top-section">
         <Link to='/'>
           <HiOutlineChevronLeft className='back-icon' />
         </Link>
-        <h2 className='details-title'>Beautiful Family Apartment</h2>
+        <h2 className='details-title'>Beautiful Family Rides</h2>
         <BsSearch className='search-icon' />
       </div>
 
       <div className="details-middle-section">
-        <img src="https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1583&q=80" alt="House" className='details-image' />
-
+        <img className='card-image' src={item.images.length > 0 && item.images[0].url}
+                  alt={item.name} />
+          <span className="label">Available</span>
+          <h2 className="car-name">{item.name}</h2>
         <div className="image-inner-text">
           <div className='left-side'>
             <div className='details-profile-container'>
@@ -33,17 +45,15 @@ const DetailsPage = () => {
                 <img src={ProfileImage} alt="Profile Image" className='details-profile-image' />
               </div>
               <div className='details-user-data'>
-                <p className="details-name">John Doe</p>
                 <p className='details-rating'>
-                <StarRating rating={4.5} />
+                <p>{item.rating}</p>
                 </p>
             </div>
             </div>
           </div>
 
           <div className='right-side'>
-            <p>$1000</p>
-            <p>per month</p>
+            <p>${item.price_per_day}</p>
           </div>
 
         </div>
@@ -51,9 +61,11 @@ const DetailsPage = () => {
 
       <div className="details-description-section">
         <h5 className='details-description-title'>About this listing</h5>
-        <p className='details-page-description'>
-          Lorem ipsum dolor sit ame consec tetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-        </p>
+        <p className="description">
+                {item.description.length > 50
+                  ? `${item.description.slice(0, 55)}...`
+                  : item.description}
+              </p>
       </div>
 
       <div className="details-read-more-arrow">
@@ -65,9 +77,25 @@ const DetailsPage = () => {
           {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
         </button>
       </div>
-
     </div>
+      ))}
+  </div>
   )
 }
+DetailsPage.propTypes = {
+  currentItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      images: PropTypes.arrayOf(
+        PropTypes.shape({
+          url: PropTypes.string.isRequired,
+          id: PropTypes.number.isRequired,
+        }),
+      ).isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
 export default DetailsPage
