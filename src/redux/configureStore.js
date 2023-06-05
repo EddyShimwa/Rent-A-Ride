9ab19f0489aca51fd6d23878d2a375170332b573
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-05 17:32:03 +0000

    register it in the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import registerSlice from './slices/registerSlice';
 import loginSlice from './slices/loginSlice';
 import rideSlice from './slices/addRideSlice';
+import fetchRideSlice from './slices/fetchRideSlice';
 
 const store = configureStore({
   reducer: {
     login: loginSlice,
     register: registerSlice,
     ride: rideSlice,
+    fetchRide: fetchRideSlice,
   }
 });
 
