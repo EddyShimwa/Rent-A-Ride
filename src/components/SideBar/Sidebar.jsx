c4f862a54f77de29a19920d51522042a9b5405e2
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-07 11:07:29 +0000

    fix logout issue

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -10,19 +10,27 @@ import { AiFillDelete } from 'react-icons/ai'
 import { CgClose } from 'react-icons/cg'
 import { BsSearch } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
+import Dialog from '../Dialog/Dialog';
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
-  
+  const [loading, setLoading] = useState(false);
+
   const navLinkClass = 'nav-link';
   const navLinkIconClass = 'nav-link-icon';
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  const removeToken = () => {
-    // if (localStorage.removeItem('token')){
-    //   // navigate('/login');
-    // }
-  }
+  const removeToken = async () => {
+    setLoading(true);
+    const token = localStorage.getItem('token');
+    if (token) {
+      localStorage.removeItem('token');
+      await navigate('/login');
+    } else {
+      await navigate('/');
+    }
+    setLoading(false);
+  };
 
   return (
       <main className={`${show && 'space-toggle'}`}>
@@ -30,7 +38,7 @@ const Sidebar = () => {
         <div className='header-toggle' onClick={() => setShow(!show)}>
           {show ? <CgClose className={navLinkIconClass} /> : <SlMenu className={navLinkIconClass} />}
         </div>
-        <p>Rent-A-Ride</p>
+        <p className='app-name'>Rent-A-Ride</p>
         <div className='header-search'>
           <BsSearch className='search-icon' />
         </div>
@@ -38,7 +46,6 @@ const Sidebar = () => {
 
       <aside className={`sidebar ${show && 'show'}`}>
         <nav className='nav'>
-          {/* {!show ? <div className='profile-logo'></div> : <div className='profile'></div>} */}
           <div className='sidebar-items-container'>
             <div className='nav-list'>
               <Link to='/my-ride' className={`nav-logo ${navLinkClass} active`}>
@@ -61,7 +68,11 @@ const Sidebar = () => {
             </div>
           </div>
 
-          <button onClick={removeToken()} className={`${navLinkClass}`}>
+          {
+            loading && <Dialog message="Loading..." isLoading={true} />
+          }
+
+          <button onClick={removeToken} className={`${navLinkClass}`}>
             <RiLogoutCircleRLine className={navLinkIconClass}/>
             <span className='nav-link-name'>Logout</span>
           </button>
