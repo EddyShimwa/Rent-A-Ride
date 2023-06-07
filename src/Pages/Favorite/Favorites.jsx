1318c7aad2464fe3668fca69614bc572472626d1
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-07 15:55:57 +0000

    remove the button for removing favorite

--- src/Pages/Favorite/Favorites.jsx
+++ src/Pages/Favorite/Favorites.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import Dialog from "../../components/Dialog/Dialog";
 import { fetchFavorites, selectFavoritesLoading, selectFavoritesError } from '../../redux/slices/fetchFavoriteSlice';
-import { removeFavorite } from '../../redux/slices/removeFavoriteSlice';
 
 const Favorites = () => {
   const dispatch = useDispatch();
@@ -23,15 +22,6 @@ const Favorites = () => {
     return <div>Error: {error}</div>;
   }
 
-  const handleRemoveFavorite = async (favoriteId) => {
-    try {
-      await dispatch(removeFavorite(favoriteId));
-      console.log('Favorite removed successfully:', favoriteId);
-    } catch (error) {
-      console.error('Failed to remove favorite:', error);
-    }
-  };
-
   return (
     <div className="favorites-container">
       <h1 className="fav-header">Favorites Rides</h1>
@@ -43,13 +33,7 @@ const Favorites = () => {
             <img src= {item.image} className="fav-car-image" alt="Favorite Car" />
             <h2 className="car-name">{item.name}</h2>
             <p className="car-desc">{item.description}</p>
-              <button className='remove'
-                onClick={() => {
-                console.log('Favorite ID:', item.id);
-                handleRemoveFavorite(item.id);
-                }}
-                > Remove Favorite
-              </button>
+
           </li>
         ))}
       </ul>
