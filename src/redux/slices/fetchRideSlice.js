79955af74002389a283769f0c466c121333a556e
EddyShimwa <shedpray16@gmail.com>
2023-06-09 18:54:09 +0200

    remove console

--- src/redux/slices/fetchRideSlice.js
+++ src/redux/slices/fetchRideSlice.js
@@ -12,7 +12,6 @@ export const fetchRides = createAsyncThunk(
         },
       });
 
-      console.log(response.data);
 
       if (response.status !== 200) {
         throw new Error(response.data.error);
